36a16cb9386c4a2935463d0cabf6f273ae1e4531
tarasbandura <taras190789@gmail.com>
2022-08-30 19:34:21 +0300

    useful finish

--- src/services/url.js
+++ src/services/url.js
@@ -1,2 +1,2 @@
-export const URL = 'http://localhost:8080';
-// export const URL = 'http://65.108.214.168:8080';
+// export const URL = 'http://localhost:8080';
+export const URL = 'http://65.108.214.168:8080';
